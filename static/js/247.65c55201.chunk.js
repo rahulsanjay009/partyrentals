a1802750b7a3654f0a9bb792fbedfa3ccb3798fe
CodingProject2024 <kanishksuggula@gmail.com>
2025-05-03 18:36:17 -0700

    Updates

---
+++ static/js/247.65c55201.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 247.65c55201.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkpc_frontend=self.webpackChunkpc_frontend||[]).push([[247],{247:(e,t,n)=>{n.r(t),n.d(t,{startStatusTap:()=>a});var r=n(146),s=n(721),o=n(384);const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,r.gv)((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const c=(0,s.a)(a);c&&new Promise((e=>(0,o.c)(c,e))).then((()=>{(0,r.bN)((async()=>{c.style.setProperty("--overflow","hidden"),await(0,s.s)(c,300),c.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=247.65c55201.chunk.js.map
