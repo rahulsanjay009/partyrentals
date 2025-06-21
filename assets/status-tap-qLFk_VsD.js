8e5a013ca48a0ed045edc45879a5d54e5b57c459
JAHNAVISRI <jahnaviyannam@gmail.com>
2025-06-21 10:11:23 -0700

    Updates

--- assets/status-tap-qLFk_VsD.js
+++
@@ -1,3 +0,0 @@
-import{r as i,f as a,a as c,w as d,s as m}from"./index-BBysh4Sa.js";import"@mui/system";import"@mui/icons-material";import"dayjs";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const h=()=>{const e=window;e.addEventListener("statusTap",()=>{i(()=>{const n=e.innerWidth,s=e.innerHeight,o=document.elementFromPoint(n/2,s/2);if(!o)return;const t=a(o);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await m(t,300),t.style.removeProperty("--overflow")})})})})};export{h as startStatusTap};
