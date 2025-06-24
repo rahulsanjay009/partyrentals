f321edb798e4f9b971d1b2b433e04a90620f0fee
JAHNAVISRI <jahnaviyannam@gmail.com>
2025-06-24 15:34:17 -0700

    Fixing manifest.json file and changing the build paths to serve the application

--- docs/pc-frontend/assets/status-tap-AxFz0EcV.js
+++ docs/pc-frontend/assets/status-tap-AxFz0EcV.js
@@ -1,3 +1,3 @@
-import{r as a,f as i,a as c,w as d,s as l}from"./index-CcD8mgb0.js";import"./vendor-Tt9FPAzf.js";/*!
+import{r as a,f as i,a as c,w as d,s as l}from"./index-DpMVRA4p.js";import"./vendor-Tt9FPAzf.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const p=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{p as startStatusTap};
